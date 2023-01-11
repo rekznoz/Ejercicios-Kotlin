9955652cce3ad12ce0da1d8425d20ac97f902342
RekZNoZ <rafikicroz@hotmail.com>
2023-01-11 12:02:26 +0100

    Ejercicio 6.6

---
+++ Ejercicio 6.6/pila.kt
@@ -0,0 +1,52 @@
+class Pila<T> {
+    private val p = mutableListOf<T>()
+
+    fun tope(): T? {
+        return if (p.isNotEmpty()) p.last() else null
+    }
+
+    fun push(element: T) {
+        p.add(element)
+    }
+
+    fun pop(): T? {
+        return if (p.isNotEmpty()) p.removeLastOrNull() else null
+    }
+
+    fun vacia(): Boolean {
+        return p.isEmpty()
+    }
+
+    override fun toString():String {
+        return p.toString()
+    }
+}
+
+fun <T> reverse(list: List<T>): List<T> {
+    val p = Pila<T>()
+    val iterator = list.iterator()
+    while (iterator.hasNext()) {
+        p.push(iterator.next())
+    }
+    val invertida = mutableListOf<T>()
+    while (!p.vacia()) {
+        invertida.add(p.pop()!!)
+    }
+    return invertida
+}
+
+fun main() {
+
+    // PILA //
+    //////////
+    var numbers = listOf("Uno", "Dos", "Tres", "Cuatro", "Cinco", "Seis", "Siete", "Ocho", "Nueve", "Cero")
+    var numbersRev = reverse(numbers)
+
+    if (!listOf("four", "three", "two", "one").equals(numbersRev))
+        println("Error")
+    else
+        println("Correcto")
+
+    println(numbersRev)
+
+}
